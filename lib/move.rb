a83d52bd89ac1566fee33f06bcff239d6a60540b
Angie Kassab <anasab12@gmail.com>
2018-05-06 21:30:18 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,11 +7,22 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
+<<<<<<< HEAD
   user_input.to_i-1
 end
 
 def move(board, index,character="X")
   board[index]=character
 end
+=======
+  user_input=gets.chomp-1 
+  
+end
+
+def move(board, index,"X")
+  board[index]="X"
+  return board 
+end 
+>>>>>>> 6a855601d9926d5b8da0ad4a1821ecb59a3dd3f1
 
 # code your input_to_index and move method here!
